426c4e5bb8638cb0fa099b37f2f7857602438dab
Archisha Chatterjee <archisha.chatterjee.btech2024@sitpune.edu.in>
2025-07-17 00:12:30 -0400

    Create Exp4A.cpp

---
+++ Exp4A.cpp
@@ -0,0 +1,39 @@
+// EXPERIMENT : 4A : Using Bitwise Operators
+// Archisha Chatterjee 
+// PRN : 24040123021
+
+// AND  (&)
+// OR  (|)
+// XOR (^)
+// NOT (~)
+// LS (<<no_of_bits)
+// RS (>>no_of_bits)
+
+#include <iostream>
+using namespace std;
+int main() {
+	int a = 2;
+	int b = 7;
+	int AND = a&b;
+	int OR = a|b;
+	int XOR = a^b;
+	int NOT_a = ~a;
+	int NOT_b = ~b;
+	int LS_a = a<<1;
+	int RS_a = a>>1 ;
+	int LS_b = b<<1;
+	int RS_b = b>>1;
+    
+	cout << "a = 2" << endl << "b = 7" << endl;
+	cout << "AND : " << AND << endl ;
+	cout << "OR : "  << OR << endl;
+	cout << "XOR : " << XOR << endl;
+	cout << "NOT a : " << NOT_a << endl;
+	cout << "NOT b : " << NOT_b << endl;
+	cout << "Left Shift a : " << LS_a << endl;
+	cout << "Right Shift a : " << RS_a << endl;
+	cout << "Left Shift b : "  << LS_b << endl;
+	cout << "Right Shift b : " << RS_b << endl;
+
+	return 0;
+}
